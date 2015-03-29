2e3e148df7567ca4c56532a00cb4ad13e1bb1fd7
itschn <lofd23@yahoo.de>
2015-03-29 19:49:28 +0200

    Fixing typo

--- app/controllers/bootsy/application_controller.rb
+++ app/controllers/bootsy/application_controller.rb
@@ -3,5 +3,17 @@ module Bootsy
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
+    
+    def mimetype_icon_css_class(mimetype)
+      return unless mimetype
+    
+      case mimetype.split('/').first
+        when 'image' then 'fa fa-file-image-o'
+        when 'pdf' then 'fa fa-file-pdf-o'
+        when 'zip', 'rar' then 'fa fa-file-archive-o'
+        when 'application' then mimetype_icon_css_class(mimetype.split('/').last)
+        when 'video' then 'fa fa-file-video-o'
+      end
+    end
   end
 end
